077e4167d4d58924a020e69fc78fb5063a7ba64e
Clodéric Mars <cloderic@ai-r.com>
2021-01-21 12:17:40 +0100

    Step 4 - Initializing the heuristic agent as a copy of the random agent

--- 4-heuristic-player/random_agent/main.py
+++ 4-heuristic-player/random_agent/main.py
@@ -46,13 +46,44 @@ async def random_agent(actor_session):
             for message in final_data.messages:
                 (sender, message) = message
                 print(f"'{actor_session.name}' received a final message from '{sender}': - '{message}'")
+
+async def heuristic_agent(actor_session):
+    actor_session.start()
+
+    async for event in actor_session.event_loop():
+        if "observation" in event:
+            observation = event["observation"]
+            print(f"'{actor_session.name}' received an observation: '{observation}'")
+            action = PlayerAction(move=random.choice(MOVES))
+            actor_session.do_action(action)
+        if "reward" in event:
+            reward = event["reward"]
+            print(f"{actor_session.name} received a reward for tick #{reward.tick_id}: {reward.value}/{reward.confidence}")
+        if "message" in event:
+            (sender, message) = event["message"]
+            print(f"'{actor_session.name}' received a message from '{sender}': - '{message}'")
+        if "final_data" in event:
+            final_data = event["final_data"]
+            for observation in final_data.observations:
+                print(f"'{actor_session.name}' received a final observation: '{observation}'")
+            for reward in final_data.rewards:
+                print(f"{actor_session.name} received a final reward for tick #{reward.tick_id}: {reward.value}/{reward.confidence}")
+            for message in final_data.messages:
+                (sender, message) = event["message"]
+                print(f"'{actor_session.name}' received a final message from '{sender}': - '{message}'")
+
 async def main():
-    print("Random-Agent actor service starting...")
+    print("Random & Heuristic agents service up and running.")
 
     context = cogment.Context(cog_settings=cog_settings, user_id="rps")
     context.register_actor(
         impl=random_agent,
         impl_name="random_agent",
+        actor_classes=["player",])
+
+    context.register_actor(
+        impl=heuristic_agent,
+        impl_name="heuristic_agent",
         actor_classes=["player",])
 
     await context.serve_all_registered(port=9000)
