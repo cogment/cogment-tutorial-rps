34b381a0b2afbc831c6f5813eaad0949f7d6a1b5
Clodéric Mars <cloderic@ai-r.com>
2021-05-11 17:02:37 -0400

    Improving the quick test script

---
+++ scripts/quick_test.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+ROOT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)
+
+set -o errexit
+
+for d in "$ROOT_DIR"/*-*/; do
+  (
+    cd "$d" || exit
+    printf "\n******** TESTING \"%s\" ********\n\n" "$d"
+    cogment run generate
+    cogment run build
+    cogment run start &
+    sleep 20
+    cogment run client &
+    sleep 10
+    cogment run stop
+    sleep 10
+    docker-compose kill
+  )
+done
